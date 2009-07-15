137c664cfcb9107bb3329851efe65f52b3f2036d
Andrew Hutchings <andrew.hutchings@gmail.com>
2009-07-15 23:31:18 +0000

    Database connection parameters.

--- config.inc.php
+++ config.inc.php
@@ -1,15 +1,15 @@
 <?php
-  
+
 // replace username, password and database-name with your details
 
-	$mainconnection =MYSQL_CONNECT("localhost","username","password") OR DIE("Unable to connect to database");  
+	$mainconnection =MYSQL_CONNECT("localhost","seokit","seokit") OR DIE("Unable to connect to database");
 
-	@mysql_select_db("database-name") or die("Unable to select the database");
+	@mysql_select_db("seokit") or die("Unable to select the database");
 
 // api key available from http://developer.yahoo.com/wsregapp/index.php
 // more details here http://developer.yahoo.com/search/
 
 	$yahoo_api_key="xxxxxxxxx";
 	$scriptlocation="http://www.site.com/linkanalysis";
-	
+
 ?>
